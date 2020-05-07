b32d74068383f6da5ab6f20225e385430ba04a03
Adnan <adnan172203@gmail.com>
2020-05-07 18:09:17 +0600

    added bcrypt for password hashing

--- models/user.js
+++ models/user.js
@@ -1,38 +1,56 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const uuidv1 = require('uuid/v1');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 32,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  hashed_password: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-    trim: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
 
-  salt: String,
+    salt: String,
 
-  role: {
-    type: Number,
-    default: 0,
+    role: {
+      type: Number,
+      default: 0,
+    },
+
+    history: {
+      type: Array,
+      default: [],
+    },
   },
-  
-  history: {
-    type: Array,
-    default: [],
-  },
-},{timestamps:true});
+  { timestamps: true }
+);
+
+
+
+//hash password
+userSchema.pre('save', async function (next) {
+  const hashedPassword = await bcrypt.hash(this.password, 10);
+  if (this.isModified('password')) {
+    this.password = hashedPassword;
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
